1e5da4c554ce5da7a0f0d55c78839faef3d8604a
Gollyx Orchestrator <orchestrator@golly.life>
2026-03-09 07:38:11 +0000

    Automatic deploy of integration at 2026-03-09-07-38-11

--- maps.js
+++ maps.js
@@ -93,7 +93,7 @@
 
         var mapRowElem = document.getElementById('row-maps');
 
-        let mapsUrl = this.mapsApiUrl + '/maps/vii/' + this.season0;
+        let mapsUrl = this.mapsApiUrl + '/maps/star-vii/' + this.season0;
         fetch(mapsUrl)
         .then(res => res.json())
         .then((mapsApiResult) => {
